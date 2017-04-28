7bd1104b98a7c403bb127603afb16100730518c7
udemyjava <detroitmillionare@hotmail.com>
2017-04-28 11:33:19 -0400

    First Commit

---
+++ If Statement/src/IfStatement.java
@@ -0,0 +1,21 @@
+
+public class IfStatement {
+	public static void main(String[] args) {
+		
+		int myInt = 15; 
+		
+		if(myInt < 10) {
+			System.out.println("Yes, its true!");
+		}
+		
+		else if(myInt > 20) {
+			System.out.println("No, its false!");
+		}
+		
+		else {
+			
+			System.out.println("None of the above.");
+		}
+		
+	}
+}
